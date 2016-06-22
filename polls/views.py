4fe2585e353e0a3c6a1f4ebbeb0d4bf0f6e7ac53
vinatgit <vinayakaggarwal97@gmail.com>
2016-06-22 08:49:55 +0530

    Static files added

--- polls/views.py
+++ polls/views.py
@@ -14,7 +14,7 @@ class IndexView(generic.ListView):
 
 	def get_queryset(self):
 		return Question.objects.filter(
-			pub_date__lte)=timezone.now()
+			pub_date__lte=timezone.now()
 			).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
